7956485239cc5b866115afbba2a554e0d926c204
adnan <adnanabdulle@adnans-mac-mini.home>
2015-01-05 12:15:02 +0000

    server running/routes updated
    server now runs without =DEBUG from express generator. Routes also
    updated. Now middleware is run to check whether user is authorised for
    page requested or not.

--- routes/index.js
+++ routes/index.js
@@ -38,9 +38,21 @@ router.get('/logout', function (req, res) {
   res.redirect('/');
 });
 
-router.get('/ping', function (req, res) {
-
-res.send('pongo', 200);
+router.get('/ping',isLoggedIn, function (req, res) {
+  res.redirect('/YouareAuthorised');
 });
+
+
+// route middleware to make sure a user is logged in. Use for every route that needs authentication
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/notAllowed');
+  }
+
 
 module.exports = router;
